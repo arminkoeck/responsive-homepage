29f2beaf2d90ebc451c091df749437ae5f70222e
arminkoeck <armin.koeck@gmail.com>
2024-07-18 17:15:46 +0200

    Finish responsive website project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./style.css";
+
+// .tablet-name {
+//     position: relative;
+//     display: block;
+//     color: white;
+//     font-size: 40px;
+//   }
+
+//   .mobile-desktop-name {
+//     display: none;
+//   }
+
+//   .header-container {
+//     display: block;
+//   }
+
+//   .header-image-container {
+//     max-width: 400px;
+//     float: left;
+//     margin: 0 20px;
+//     display: flex;
+//     flex-direction: column;
+//   }
+
+//   .header-content {
+//     margin: 0 0 0 60px;
+//     border-radius: 5px;
+//   }
